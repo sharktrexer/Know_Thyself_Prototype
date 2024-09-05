3f9aa0c98ff3e2ae4c59a7366a0b7ed6b6b40c07
sharktrexer <sharktrexer@gmail.com>
2024-09-05 16:54:29 -0700

    created achievements per ending and secret option

---
+++ src/com/thyself/AccomplishmentBuilder.java
@@ -0,0 +1,46 @@
+package com.thyself;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AccomplishmentBuilder {
+
+	public static ArrayList<Accomplishment> copStoryAccomps = new ArrayList<>(Arrays.asList(
+			
+			new Accomplishment("arrested_end", "You Got Arrested...", 1),
+			
+			new Accomplishment("work_raid_end", "Gluttony", 1),
+
+			new Accomplishment("home_ponder_end", "Everything, Everywhere", 1),
+			
+			new Accomplishment("work_end", "A Little Too Hard Working", 1),
+			
+			new Accomplishment("party_end", "PARTY TIME!", 1),
+			
+			new Accomplishment("party_end_rehire", "A Good Cop All Along", 1),
+			
+			new Accomplishment("home_news_end", "Let The World Burn", 1),
+			
+			new Accomplishment("home_end", "Welcome Home!", 1),
+			
+			new Accomplishment("fake_crime", "The Tales of Ancient Acres", 2),
+			
+			new Accomplishment("helpful", "Bake Sale Enthusiast", 2),
+			
+			new Accomplishment("kinda", "Good Intentions", 2),
+			
+			new Accomplishment("rewarded", "Pride", 2),
+			
+			new Accomplishment("blame", "No Rest For The Rookie", 2),
+			
+			new Accomplishment("wonder", "Head In The Clouds", 2),
+			
+			new Accomplishment("no_big_deal", "NOW It's a Big Deal", 2),
+			
+			new Accomplishment("questioning", "Not The Best Idea...", 2),
+			
+			new Accomplishment("lie", "20/20 Vision", 2)
+			
+			));
+	
+}
